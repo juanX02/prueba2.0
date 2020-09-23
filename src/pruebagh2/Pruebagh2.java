bb83448b4c0dedc7a4378d170b967d583e7b8fe1
juanX02 <71611355+juanX02@users.noreply.github.com>
2020-09-22 23:54:01 -0500

    pruebas 2 conexion  github

---
+++ src/pruebagh2/Pruebagh2.java
@@ -0,0 +1,17 @@
+
+package pruebagh2;
+
+/**
+ *
+ * @author JuanX
+ */
+public class Pruebagh2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola ");
+    }
+    
+}
